0c41498a2265bce274fa02630172499ff5f100e8
ViktorIukhlin <iukhlin.v@gmail.com>
2024-08-11 15:50:40 +0300

    [chore] Created constants for the game

---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+export const START_BALANCE = 5000;
+export const MINIMUM_BET = 500;
+export const MAXIMUM_BET = null;
+export const MAX_SELECTABLE_POSITIONS = 2;
+export const WINNING_RATE_FOR_ONE_POSITION = 14;
+export const WINNING_RATE_FOR_TWO_POSITIONS = 3;
